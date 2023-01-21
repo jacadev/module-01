ab2683bd213470bbf398c6ad0c1197935f0523e2
Jesus <jaca-btc@gmail.com>
2023-01-21 14:22:04 -0500

    A fix in nFactorial function

--- 04-EstructuraDeDatos-I/homework/homework.js
+++ 04-EstructuraDeDatos-I/homework/homework.js
@@ -2,7 +2,8 @@
 
 // EJERCICIO 1
 function nFactorial(n) {
-   if (n === 0 || n === 1) return 1   
+   if (n === 0 || n === 1) return 1;
+   else if (n < 0) return 0; 
    return n * nFactorial(n - 1)
 }
 
