89f7cd9782d35076b1e6319ac6f82f2a67acbe6b
Jesus <jaca-btc@gmail.com>
2023-01-21 18:46:29 -0500

    nFibonacci function is done

--- 04-EstructuraDeDatos-I/homework/homework.js
+++ 04-EstructuraDeDatos-I/homework/homework.js
@@ -8,7 +8,11 @@ function nFactorial(n) {
 }
 
 // EJERCICIO 2
-function nFibonacci(n) {}
+function nFibonacci(n) {
+   if (n === 0) return 0;
+   else if (n === 1) return 1;    
+   return nFibonacci(n - 1) + nFibonacci(n - 2);
+}
 
 // EJERCICIO 3
 function Queue() {}
